9c4641dbf050f826eaf58ab10f220f46eca4ea09
seikou <seikou615@gmail.com>
2023-08-29 19:36:40 +0100

    Add: `testimonialData` constant

---
+++ src/Constants/testimonialsData.ts
@@ -0,0 +1,24 @@
+const testimonialsData = [
+  {
+    name: 'Colton Smith',
+    message:
+      ' We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!',
+    image: 'images/image-colton.jpg'
+  },
+
+  {
+    name: 'Irene Roberts',
+    message:
+      'Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.',
+    image: 'images/image-irene.jpg'
+  },
+
+  {
+    name: 'Anne Wallace',
+    message:
+      'Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!',
+    image: 'images/image-anne.jpg'
+  }
+]
+
+export { testimonialsData }
