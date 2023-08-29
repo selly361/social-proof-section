d59ca87d17dd699a09a800a7a30bb50b1026c10e
seikou <seikou615@gmail.com>
2023-08-29 19:53:35 +0100

    Update: imported styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import 'Styles/index.scss'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
